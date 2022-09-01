ac064040d0637275ca830e60dc4a2b17065a9ac1
MurilloEdson <>
2022-09-01 10:06:59 -0400

    Main class initiated

--- CSC325_Fall22_LabDemo/src/main/java/edu/farmingdale/csc325_fall22_labdemo/NewClass.java
+++ CSC325_Fall22_LabDemo/src/main/java/edu/farmingdale/csc325_fall22_labdemo/NewClass.java
@@ -3,6 +3,10 @@ package edu.farmingdale.csc325_fall22_labdemo;
 /**
  * @author Edson
  */
-public class NewClass {
-    
+public class NewClass 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
 }
